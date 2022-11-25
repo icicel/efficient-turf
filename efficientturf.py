6ea377367306350da5c2e869a5e8999c175fe0f4
icicel <isakham@chalmers.se>
2022-11-25 16:34:27 +0100

    Numerical IDs for zones

--- efficientturf.py
+++ efficientturf.py
@@ -34,28 +34,41 @@ dumpname = "turf"
 
 ### PREPARE DATA
 
-import time, requests, random, importlib, pickle
-from datetime import datetime
+import time, datetime, requests, random, importlib, pickle
 if debug_print_zone_data:
     maxdistance = 0
 else:
     maxdistance = speed * int(input("Time in minutes:\n> "))
 time_at_start = time.time()
 
-# import and check validity of data
+# import and enumerate data 
 try:
     data = importlib.import_module("data_" + data_set)
 except ModuleNotFoundError:
     quit(print("ERROR: data_" + data_set + ".py not found"))
-defined_zones = data.zlist + data.clist
-if data.start_zone not in defined_zones:
+zid = {}
+zname = {}
+for i, zone in enumerate(data.zlist + data.clist): # generate numerical ids for zones
+    zid[zone] = i
+    zname[i] = zone
+zlist = [zid[zone] for zone in data.zlist]
+clist = [zid[zone] for zone in data.clist]
+defined_zones = zlist + clist
+start_zone = zid[data.start_zone]
+end_zone = zid[data.end_zone]
+blacklist = [zid[zone] for zone in data.blacklist]
+whitelist = data.whitelist
+connections = [(zid[z1], zid[z2], d) for z1, z2, d in data.connections]
+
+# check validity
+if start_zone not in defined_zones:
     quit(print("ERROR: Start zone is invalid"))
-if data.end_zone not in defined_zones:
+if end_zone not in defined_zones:
     quit(print("ERROR: End zone is invalid"))
-for zone in data.blacklist:
+for zone in blacklist:
     if zone not in defined_zones:
         print("WARNING: Blacklist contains invalid zone name: " + zone)
-for zone1, zone2, _ in data.connections:
+for zone1, zone2, _ in connections:
     if zone1 not in defined_zones:
         print("WARNING: Connection contains invalid zone name: " + zone1)
     if zone2 not in defined_zones:
@@ -63,7 +76,14 @@ for zone1, zone2, _ in data.connections:
 
 # converts an ISO8601 time format string to seconds (since epoch, probably? doesn't matter)
 def str2time(s):
-    return datetime.strptime(s, "%Y-%m-%dT%H:%M:%S%z").timestamp()
+    return datetime.datetime.strptime(s, "%Y-%m-%dT%H:%M:%S%z").timestamp()
+
+# converts a list of zone ids to a list of zone names
+def znames(zidlist):
+    return [zname[zone] for zone in zidlist]
+# the same but for dictionary keys
+def znamed(ziddict):
+    return {zname[zone]: ziddict[zone] for zone in ziddict}
 
 # get round info with the turf API (specifically, how many hours are left)
 try:
@@ -92,10 +112,10 @@ if debug_print_zone_data == True: # debug
     if not has_connection:
         quit(print("ERROR: Can't print zone data without a connection"))
     print("name\tage\tpotential days\tpts on take\tpts per hour\tpts total\ttake age\trevisit pts\tneutral pts")
-    zone_datas = requests.post("http://api.turfgame.com/v4/zones", json=[{"name": zone} for zone in data.zlist]).json()
+    zone_datas = requests.post("http://api.turfgame.com/v4/zones", json=[{"name": zname[zone]} for zone in zlist]).json()
     for i, zone_data in enumerate(zone_datas):
-        zone = data.zlist[i]
-        line = [zone]
+        zone = zlist[i]
+        line = [zname[zone]]
         hours_existed = (time.time() - str2time(zone_data["dateCreated"])) / 3600
         potential_hours = min(hours_existed / zone_data["totalTakeovers"], round_hours_left)
         line.append(int(hours_existed / 24))
@@ -123,10 +143,10 @@ if debug_print_zone_data == True: # debug
         print("\t".join(line).replace(".",","))
     quit()
 elif has_connection: # not debug
-    zone_datas = requests.post("http://api.turfgame.com/v4/zones", json=[{"name": zone} for zone in data.zlist]).json()
+    zone_datas = requests.post("http://api.turfgame.com/v4/zones", json=[{"name": zname[zone]} for zone in zlist]).json()
     zone_points = {}
     for i, zone_data in enumerate(zone_datas):
-        zone = data.zlist[i]
+        zone = zlist[i]
 
         hours_existed = (time.time() - str2time(zone_data["dateCreated"])) / 3600
         potential_hours = min(hours_existed / zone_data["totalTakeovers"], round_hours_left) # average hours before the zone is lost
@@ -141,7 +161,7 @@ elif has_connection: # not debug
                     zone_points[zone] = 0
         else: # no currentOwner - gives neutral bonus instead
             zone_points[zone] += 50
-    for zone in data.clist: # crossings don't give points
+    for zone in clist: # crossings don't give points
         zone_points[zone] = 0
     with open(dumpname + ".pk", "wb") as file:
         pickle.dump(zone_points, file)
@@ -157,44 +177,44 @@ else:
     print("WARNING: No connection, loading backup file")
     with open(dumpname + ".pk", "rb") as file:
         zone_points = pickle.load(file)
-    for zone in data.clist:
+    for zone in clist:
         zone_points[zone] = 0
-    for zone in data.zlist:
+    for zone in zlist:
         if zone not in zone_points:
             zone_points[zone] = 0
 
 # collect all zones (and their connections)
 zones = {}
-if data.whitelist:
+if whitelist:
     for zone in zone_points:
-        if zone in data.blacklist:
+        if zone in blacklist:
             zones[zone] = []
 else:
     for zone in zone_points:
-        if zone not in data.blacklist:
+        if zone not in blacklist:
             zones[zone] = []
-for zone1, zone2, length in data.connections:
+for zone1, zone2, length in connections:
     if zone1 in zones and zone2 in zones:
         zones[zone1].append((zone2, length))
         zones[zone2].append((zone1, length))
 
 # filter out (kill) zones that can't be reached from start_zone
-if data.blacklist:
+if blacklist:
     kill_list = list(zones.keys())
     def survive(zone):
         kill_list.remove(zone)
         for neighbor, _ in zones[zone]:
             if neighbor in kill_list:
                 survive(neighbor)
-    survive(data.start_zone)
-    if data.end_zone in kill_list:
+    survive(start_zone)
+    if end_zone in kill_list:
         quit(print("\nERROR: No possible paths found through non-blacklisted zones"))
-    print("Unreachable zones were deleted: " + ", ".join(kill_list))
+    print("Unreachable zones were deleted: " + ", ".join(znames(kill_list)))
     for zone in list(zones.keys()): # remake zones without these unreachable zones
         if zone in kill_list:
             del zones[zone]
         zones[zone] = []
-    for zone1, zone2, length in data.connections:
+    for zone1, zone2, length in connections:
         if zone1 in zones and zone2 in zones:
             zones[zone1].append((zone2, length))
             zones[zone2].append((zone1, length))
@@ -216,7 +236,7 @@ zone_distance_between = {}
 # fill out above dictionaries
 for endzone in zones:
     endpaths = []
-    if zone_points[endzone] != 0 or endzone == data.start_zone or endzone == data.end_zone:
+    if zone_points[endzone] != 0 or endzone in [start_zone, end_zone]:
         # sub-algorithm, calculates the fastest path from "endzone" to every other zone
         endzonedistance = {endzone: 0}
         endzonepathto = {endzone: [endzone]}
@@ -232,7 +252,7 @@ for endzone in zones:
                     if connection[0] not in endzonedistance:
                         endpaths.append([endpath[0] + connection[1]] + endpath[1:] + [connection[0]])
             endpaths.pop(0)
-            if endpaths == []:
+            if not endpaths:
                 break
         # collects the information in the big dictionaries
         zone_distance_between[endzone] = endzonedistance
@@ -262,7 +282,7 @@ def get_artipoints(zone, d):
             low[zone] = min(low[zone], depth[child])
     if (zone in parent and is_artipoint) or (zone not in parent and children > 1):
         artipoints.append(zone)
-get_artipoints(data.start_zone, 0)
+get_artipoints(start_zone, 0)
 
 # finds articulation points connected to 3+ blocks specifically
 arti3points = []
@@ -286,7 +306,7 @@ for zone in artipoints:
 # debug stuff
 if debug_show_artipoints == True:
     print("\nArtipoint stuff:")
-    print(artipoints, arti3points, block_amount, sep="\n")
+    print(znames(artipoints), znames(arti3points), znamed(block_amount), sep="\n")
 if debug_central_zones == True:
     tl = []
     for startzone in zone_distance_between:
@@ -295,15 +315,15 @@ if debug_central_zones == True:
             c += zone_distance_between[startzone][endzone]
         tl.append([c, startzone])
     tl.sort(key = lambda x:x[0])
-    print("\nMost central zone: " + tl[0][1] + "\nMost isolated zone: " + tl[-1][1])
+    print("\nMost central zone: " + zname[tl[0][1]] + "\nMost isolated zone: " + zname[tl[-1][1]])
 if debug_show_zone_points == True:
     print("\nZone points:")
-    print(dict(sorted(zone_points.items(), key=lambda x: x[1], reverse=True)))
+    print(dict(sorted(znamed(zone_points).items(), key=lambda x:x[1], reverse=True)))
 
 if maxdistance == 0: # no reason to even start the loop if this is the case
     quit(print("ERROR: Distance is zero, aborting"))
 # initialize paths! let's go!
-paths = [[0, zone_points[data.start_zone], data.start_zone, 0, data.start_zone]]
+paths = [[0, zone_points[start_zone], start_zone, 0, start_zone]]
 
 
 
@@ -337,7 +357,7 @@ while True:
             # and now, some optimizations!
             # if one of these situations happen it means that there (probably) exists another, better path, and this path is removed
 
-            if maxdistance - distance < zone_distance_between[data.end_zone][new_zone]: # if it can't be finished without exceeding the time limit
+            if maxdistance - distance < zone_distance_between[end_zone][new_zone]: # if it can't be finished without exceeding the time limit
                 continue
             if new_zone == last_captured_zone: # if it returns to a zone without visiting any point-giving zones
                 continue
@@ -346,7 +366,7 @@ while True:
             used_connections = zip(path_zones, path_zones[1:])
             if (last_zone, new_zone) in used_connections: # if it uses the same connection in the same direction twice
                 continue
-            if new_zone in path_zones and new_zone != data.end_zone: # if it returns to a zone
+            if new_zone in path_zones and new_zone != end_zone: # if it returns to a zone
                 if path_zones.count(new_zone) > 1 and new_zone not in arti3points: # if the zone has been visited three times
                     continue
                 if (new_zone, last_zone) not in used_connections and new_zone not in artipoints: # if it doesn't return to the zone via the connection it left it with
@@ -356,7 +376,7 @@ while True:
                 toadd.append([distance, points + zone_points[new_zone], new_zone, 0] + path_zones + [new_zone])
             else:
                 toadd.append([distance, points, last_captured_zone, last_captured_distance + new_distance] + path_zones + [new_zone])
-        if last_zone == data.end_zone and points >= best_points:
+        if last_zone == end_zone and points >= best_points:
             best_points = points
             finished_paths.append(path)
 
@@ -365,7 +385,8 @@ while True:
     if paths == []: # if all paths are finished
         break
     if debug_show_random_path:
-        print(random.choice(paths))
+        p = random.choice(paths)
+        print(p[:4] + znames(p[4:]))
 
     print("At step " + str(step) + " there were " + str(len(paths)) + " active paths and " + str(len(finished_paths)) + " finished")
     step += 1
@@ -395,10 +416,11 @@ for path in finished_paths:
 
 if debug_unused_connections == True:
     # collects all connections that the finished paths use
-    used_connections = set()
+    used_connections = []
     for path in finished_paths:
         for connection in zip(path[2:], path[3:]):
-            used_connections.add(set(connection))
+            if connection not in used_connections:
+                used_connections.append(set(connection))
     
     # gathers all connections not in used_connections
     unused_connections = []
@@ -407,7 +429,7 @@ if debug_unused_connections == True:
             connection = {zone, neighbor}
             if connection not in used_connections and connection not in unused_connections:
                 unused_connections.append(connection)
-    unused_connections = [tuple(x) for x in unused_connections]
+    unused_connections = [tuple(znames(sorted(list(x)))) for x in unused_connections]
     unused_connections.sort()
     print("\nUnused paths:")
     print(unused_connections)
@@ -436,19 +458,19 @@ if len(finished_paths) != 1:
         print("{} points, {} min: {}".format(
             round(path[1], 1),
             str(int(path[0] / speed)),
-            "-".join([zone.upper() for zone in path[2:] if zone in data.zlist])))
+            "-".join([zname[zone].upper() for zone in path[2:] if zone in zlist])))
 
 # write the result
 print("\nBest path:")
 best = finished_paths[0]
 best_zones = []
-for i, zone in enumerate(best[2:]):
-    if zone in data.zlist and zone not in best[2:i+2]:
-        best_zones.append(zone.upper())
+for i, zone in enumerate(best[2:], start=2):
+    if zone in zlist and zone not in best[2:i]:
+        best_zones.append(zname[zone].upper())
 print("{} points, {} zones, {} min: {}\n\nTechnical representation:\n{}\n\nThe process took {} seconds".format(
     str(round(best[1], 1)),
     str(len(best_zones)),
     str(int(best[0] / speed)),
     "-".join(best_zones),
-    str(best),
+    str(best[:2] + znames(best[2:])),
     str(round(time.time() - time_at_start, 2))))
